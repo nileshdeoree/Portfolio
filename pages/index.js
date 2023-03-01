4d74642c187b69f79ad89278cc71de6660311355
nilesh <deorenilesh@kbtcoe.org>
2023-03-01 13:55:47 +0530

    adding text utils

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
               </div>
             </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              <img class="object-cover object-center rounded" alt="hero" src="nil.jpg" />
+              <img class="object-cover object-center rounded " alt="hero" src="nil.jpg" />
             </div>
           </div>
         </section>
@@ -96,6 +96,30 @@ export default function Home() {
         <section class="text-gray-600 body-font">
           <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap -m-4">
+              <div class="p-4 md:w-1/3">
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="Screenshot_20230301_011522.png" alt="blog" />
+                  <div class="p-6">
+                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">JAVASCRIPT, REACT, BOOTSTRAP</h2>
+                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Text Editor</h1>
+                    <p class="leading-relaxed mb-3">This website is very useful for the editing of text such as removing extra spaces, converting to uppercase, converting to lowercase.</p>
+                    <div class="flex items-center flex-wrap ">
+                      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 ml-6" href='https://github.com/nileshdeoree/TextUtils' target="_blank" rel="noreferrer">View Code
+                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                      <a class="text-red-700 inline-flex items-center md:mb-2 lg:mb-0  pl-20" href='https://textutils1.vercel.app/' target="_blank" rel="noreferrer" >Go Live
+                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div class="p-4 md:w-1/3">
                 <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                   <img  class="lg:h-48 md:h-36 w-full object-cover object-center" src="weather.png" alt="blog" />
@@ -241,6 +265,7 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+
 
 
             </div>
